431d6f8b2eb739dbe331f42b1de48a59d0c7b5b0
Ben Kaiser <bkaiser94@gmail.com>
2018-12-31 00:31:23 -0500

    probably a bunch of changes I never pushed. and new file to convert the colon-separated values to decimals

---
+++ make_decimal_coords.py
@@ -0,0 +1,13 @@
+"""
+Created by Ben Kaiser (UNC-Chapel Hill) 2018-12-31
+
+"""
+
+
+
+import numpy as np
+from astroquery.gaia import Gaia
+import astropy.units as u
+import astropy.coordinates as coord
+from astropy.table import Table
+ 
