0085f313edf7459409fbb1afef05ee2912db0525
Ben Kaiser <bkaiser94@gmail.com>
2019-05-17 16:34:29 -0400

    created check_gentile_fusillo.py to determine if objects fall into the Gaia WD catalogue that most people will be using; it's not functional yet in any way; it's literally just a filename and doc string

---
+++ check_gentille_fusillo.py
@@ -0,0 +1,11 @@
+"""
+Created by Ben Kaiser (UNC-Chapel Hill) 2019-05-17
+
+Check an existing list of Gaia objects to see if any of them might also fall into the Gentillo-Fusillo et al. 2019
+catalogue, without actually doing a cross match with the catalogue.
+
+Effectively, this should be looking at all of the data cuts implemented in that paper and seeing if our objects pass
+them, meaning that Nicola or others would most likely be looking at these objects.
+
+
+"""
