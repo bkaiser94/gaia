1c30cd96a60719f8f2b3848487f6200ee4b633fe
Ben Kaiser <bkaiser94@gmail.com>
2019-09-17 10:40:40 -0400

    Created plot_param_spaces.py to plot Gaia objects with error bars in various parameter spaces.

---
+++ plot_param_spaces.py
@@ -0,0 +1,12 @@
+"""
+Created by Ben Kaiser (UNC-Chapel Hill) 2019-09-17
+
+
+Plot a given Gaia target list as a scatter plot with error bars for a variety of parameters. Will also eventually include 
+quality cuts overlaid perhaps, and maybe a background plotting version.
+
+Basically a hybrid of plot_alt_cmd.py and plot_data_checks.py
+
+Actually this might end up being like plot_alt_cmd.py essentially replacing plot_gaia_cmd.py and potentially 
+displacing plot_data_checks.py
+"""
