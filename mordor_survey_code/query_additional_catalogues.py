64381d1d9b215eb80efaeb8dbcec4733f9dab7c5
Ben Kaiser <bkaiser94@gmail.com>
2025-01-19 21:18:39 -0500

    Working on Xmatch/joining tables for the MORDOR survey. Made files to that effect. Probably other changes I forgot to push as always.

---
+++ mordor_survey_code/query_additional_catalogues.py
@@ -0,0 +1,18 @@
+"""
+Created by Ben Kaiser (UNC-Chapel Hill) 2025-01-18.
+
+This script should take Gaia IDs (probably from DR3, but maybe DR2 because I'm stupid) and then query for PanSTARRS photometry for those objects.
+It should also do the same for SDSS, 2MASS, UKIDSS, and maybe others if at all possible.
+
+
+
+
+This example using PanSTARRS in Gaia is going to be my reference method for this. Hopefully it will translate. 
+
+
+https://allendowney.github.io/AstronomicalData/05_join.html
+
+Unfortunately he used DR2, so this will require some finagling to get working with DR3 probably... and I still haven't actually figured out getting the DR3 data... god damn it.
+
+
+"""
